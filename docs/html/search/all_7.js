63a50f0ac4c075e8729ec3b1a7c61c0d9ac0d82a
sandyre <aleksanteri@niuitmo.ru>
2016-06-19 13:32:58 +0300

    docs updated

--- docs/html/search/all_7.js
+++ docs/html/search/all_7.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['geometrytype',['GeometryType',['../class_c_a_d_geometry.html#a0515ed61d3514d528f78b8ba6c4fc5f3',1,'CADGeometry']]],
   ['getgeometry',['getGeometry',['../class_c_a_d_file.html#a1c841beaf00d33fe888d5f750841829b',1,'CADFile::getGeometry()'],['../class_d_w_g_file_r2000.html#a8838da9ee3111f5aba1451aa59528760',1,'DWGFileR2000::getGeometry()']]],
-  ['getobject',['getObject',['../class_c_a_d_file.html#aeb68cd431afb9c5657ae7cbe06deffc6',1,'CADFile::getObject()'],['../class_d_w_g_file_r2000.html#a4538211a1567cff8e340d769e8a0d2c3',1,'DWGFileR2000::getObject()']]]
+  ['getobject',['getObject',['../class_c_a_d_file.html#a185f58e71432aa69affe49929cb44be4',1,'CADFile::getObject()'],['../class_d_w_g_file_r2000.html#acf783d35f2fc64756d38a5cb30f4885f',1,'DWGFileR2000::getObject()']]]
 ];
